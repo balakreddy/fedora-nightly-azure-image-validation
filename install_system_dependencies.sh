7a99d5ca049c33f11721862786662e1a14828728
Bala Konda Reddy M <bala12352@gmail.com>
2025-05-14 10:43:07 -0700

    Install all required dependent packages

---
+++ install_system_dependencies.sh
@@ -0,0 +1,2 @@
+# Assuming the script is run in latest fedora, install the dependencies
+sudo dnf install git gcc gobject-introspection-devel cairo-devel qemu-img libvirt-devel python3-pip python3-virtualenv -y
